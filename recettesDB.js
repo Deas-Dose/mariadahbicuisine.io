0a45ffc11c6d75fc09f76553affc03fae048e710
Hamza Chebbah <hamzachebbah9999@gmail.com>
2023-04-28 13:11:31 +0100

    fixed js, added db file

---
+++ recettesDB.js
@@ -0,0 +1,13 @@
+const recettesDB = [{
+  id: 1,
+  name: "Poulet au curry",
+  category: "plat",
+  duration: "30 minutes",
+  country: "Inde",
+  image: "https://www.cuisineaz.com/assets/recettes/720x405/2013-12-20/recipe-image-22807.jpg",
+  ingredients: ["Poulet", "Curry", "Crème fraîche", "Riz"],
+  comments: {
+    rating: 4,
+  }
+
+}]
