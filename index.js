489fecc6b2a73359f7a12522d12f8a69bcb89689
maariiaaxx <124254204+maariiaaxx@users.noreply.github.com>
2023-05-04 13:26:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Récupération du conteneur des recettes
-// Récupération du conteneur des recettes
 const recettesContainer = document.getElementById('recettes-container');
 
 // Sélection aléatoire de 3 recettes à partir du tableau d'objets recettes
@@ -23,4 +22,40 @@ recettesAleatoires.forEach(recette => {
     </div>
   `;
   recettesContainer.insertAdjacentHTML('beforeend', recetteHTML);
+});
+
+
+
+const input = document.getElementById('input1');
+input.addEventListener('keydown', function(event) {
+ 
+    const inputValue = input.value;
+
+    const searchResults = [];
+    for (let i = 0; i < recettesDB.length; i++) {
+      const recetteName = recettesDB[i].name.toLowerCase();
+      if (recetteName.includes(inputValue.toLowerCase())) {
+        searchResults.push(recettesDB[i]); 
+        console.log(recetteDB);
+      }
+    }
+
+    const searchResultsContainer = document.getElementById('searchResults');
+    searchResultsContainer.innerHTML = '';
+    for (let i = 0; i < searchResults.length; i++) {
+      const recette = searchResults[i];
+      const recetteHTML = `
+        <div class="recette">
+          <img src="${recette.image}" alt="${recette.name}">
+          <h2>${recette.name}</h2>
+          <p>${recette.category} - ${recette.country} - ${recette.duration}</p>
+          <p>${recette.global}</p>
+          <ul>
+            ${recette.ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+          </ul>
+        </div>
+      `;
+      searchResultsContainer.innerHTML += recetteHTML;
+   
+  }
 });
